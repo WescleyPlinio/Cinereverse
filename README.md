c63335911215959a791a3f31e646c7f3a3ed4d14
wescleyplinio <wescleygaldino06@gmail.com>
2026-03-17 22:23:20 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sistema back-end para gerenciamento de filmes e sessões
+Detalhes em breve
---
+++ cinema/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=120)
+
+class Filme(models.Model):
+
+    FAIXAS_ETARIAS_CHOICES = [
+        ("Livre", "Livre"),
+        ("10 anos", "10 anos"),
+        ("12 anos", "12 anos"),
+        ("14 anos", "14 anos"),
+        ("16 anos", "16 anos"),
+        ("+18", "+18"),
+    ]
+
+    titulo = models.CharField(max_length=255)
+    sinopse = models.CharField(max_length=1255)
+    genero = models.ForeignKey(Genero, on_delete="cascade")
+    ano_lancamento = models.DateField()
+    duracao_em_min = models.IntegerField()
+    faixa_etaria = models.CharField(max_length=50, choices = FAIXAS_ETARIAS_CHOICES)
+    
+class Sessao(models.Model):
+    filme = models.ForeignKey(Filme, on_delete="cascade")
+    quantidade_cadeiras = models.IntegerField(default=120)
